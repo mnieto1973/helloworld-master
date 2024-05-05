0238112163eb175a9aad58d819534917a077b2d6
Mariano Nieto Viñas <mnieto73@hotmail.com>
2024-05-05 20:45:41 +0200

    Se separa test unitario de divison por zero para que se veaa que es un test indepediente

--- test/unit/calc_test.py
+++ test/unit/calc_test.py
@@ -44,8 +44,10 @@ class TestCalculate(unittest.TestCase):
         self.assertRaises(TypeError, self.calc.divide, "2", 2)
         self.assertRaises(TypeError, self.calc.divide, 2, "2")
         self.assertRaises(TypeError, self.calc.divide, "2", "2")
+      
+    def test_divide_method_fails_with_divisor_zero(self):
         self.assertRaises(TypeError, self.calc.divide, 2, 0)
-
+        
     def test_divide_method_returns_correct_result(self):
         self.assertEqual(4, self.calc.divide( 8, 2))
         self.assertEqual(5, self.calc.divide( 15, 3))
